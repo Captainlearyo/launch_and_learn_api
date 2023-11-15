22b49c8056163b30b9fc9250b5582122d37a7a19
Captainlearyo <clayoleary@gmail.com>
2023-11-15 13:28:37 -0600

    complete story 3

--- spec/requests/api/v1/requests/user_requests/create_spec.rb
+++ spec/requests/api/v1/requests/user_requests/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Create User" do
-    it "Happy Path creates a new user when givin correct info" do
+    it "Happy Path creates a new user when givin valid info" do
       user = {
         "name": "John",
         "email": "yahoo.com",
@@ -24,4 +24,41 @@ RSpec.describe "Create User" do
 
     end
 
+    it "Sad Path creates a new user when givin invalid info :password" do
+      user = {
+        "name": "John",
+        "email": "yahoo.com",
+        "password": "1234",
+        "password_confirmation": "4321"
+      }
+
+      post "/api/v1/users", params: user, as: :json
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result).to be_a(Hash)
+      expect(result).to have_key(:error)
+      expect(result[:error]).to eq("Passwords must match")
+    
+    end
+
+    it "Sad Path creates a new user when givin invalid info :email not unique" do
+      User.create(name: "John", email: "yahoo.com", password: "1234", password_confirmation: "1234")
+
+      user = {
+        "name": "John",
+        "email": "yahoo.com",
+        "password": "1234",
+        "password_confirmation": "1234"
+      }
+
+      post "/api/v1/users", params: user, as: :json
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result).to be_a(Hash)
+      expect(result).to have_key(:error)
+      expect(result[:error]).to eq("invlalid infomation")
+    
+    end
 end
